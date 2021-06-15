3e26d71ff9569d659dca8ae7fbd00533ecb5b5dd
SALINI-04 <93289@fhss.sjp.ac.lk>
2021-06-15 15:24:50 +0530

    Updates

---
+++ precache-manifest.b7015318dd3e0b0bd5117e442eadfbdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760c4d338bab0bcd56255e538fcef585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44aaf469bceddd65d850",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44aaf469bceddd65d850",
+    "url": "/static/js/main.5b50a2ee.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "cfb56a96a1446e99b4bf160a42a9ad31",
+    "url": "/static/media/resume.cfb56a96.pdf"
+  },
+  {
+    "revision": "5bab74d18a0b4f2e3f04fe726f57970e",
+    "url": "/static/media/sali.5bab74d1.jpg"
+  }
+]);
